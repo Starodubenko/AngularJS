5026858d0dc69b018ffaa1e2872bbc57e8a6175b
Admin <Rody10@mail.ru>
2015-04-16 17:12:44 +0600

    ADD just a little code to directives.

--- src/main/webapp/script/validator/password.js
+++ src/main/webapp/script/validator/password.js
@@ -1,3 +1,60 @@
-/**
- * Created by Rody on 15.04.2015.
- */
+(function () {
+    angular.module("App")
+        .directive("password", password);
+
+    function password() {
+
+        var lastTrueRegex = {};
+
+        var regexes = {
+            week: /(?=^.{8,}$).*$/,
+            pettyWeek: /(?=^.{8,}$)(?=.*\d).*$/,
+            normal: /(?=^.{8,}$)(?=.*\d)(?=.*[a-z]).*$/,
+            prettyStrong: /(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/,
+            strong: /(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?![.\n]).*$/,
+            veryStrong: /(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?![.\n])(?=.*[!@#$%^&*]+).*$/
+        };
+
+        function forEach(data, callback) {
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    callback(key, data[key]);
+                }
+            }
+        }
+
+        return {
+            require: "ngModel",
+            restrict: 'EA',
+            link: function (scope, element, attributes, ngModel) {
+
+                ngModel.$parsers.unshift(function (textValue) {
+
+                    //alert("in parsers !");
+                    //forEach(regexes, function (key, regex) {
+                    //    $('input[password]').removeClass('ng-valid-'+key);
+                    //});
+                    forEach(regexes, function (key, regex) {
+                        if (regex.test(textValue)){
+                            lastTrueRegex.name = key;
+                            lastTrueRegex.value = true;
+                        }
+                    });
+
+                    if (lastTrueRegex.name){
+                        ngModel.$setValidity(lastTrueRegex.name, lastTrueRegex.value);
+                        return textValue;
+                    }else{
+                        ngModel.$setValidity(lastTrueRegex.name, lastTrueRegex.value);
+                        return textValue;
+                    }
+                });
+
+                ngModel.$formatters.unshift(function (modelValue) {
+                    //ngModel.$setValidity('strongPass', isValid(modelValue));
+                    return modelValue;
+                });
+            }
+        };
+    }
+})();
--- src/main/webapp/script/validator/userName.js
+++ src/main/webapp/script/validator/userName.js
@@ -1,3 +1,23 @@
-/**
- * Created by Rody on 15.04.2015.
- */
+(function(){
+    angular.module("App")
+        .directive("compareTo", compareTo);
+
+    function compareTo() {
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+
+                ngModel.$validators.compareTo = function(modelValue) {
+                    return modelValue == scope.otherModelValue;
+                };
+
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
+    }
+})();
